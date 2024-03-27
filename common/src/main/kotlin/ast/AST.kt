679afbc077f1a83a54d95186d220c2b1653af70b
RocioNavarro <56227691+RocioNavarro@users.noreply.github.com>
2024-03-27 19:55:20 -0300

    .

--- common/src/main/kotlin/ast/AST.kt
+++ common/src/main/kotlin/ast/AST.kt
@@ -9,7 +9,7 @@ data class NumberOperatorNode(val value: Double): BinaryNode // Nro ej: 5
 data class BinaryOperationNode(val symbol: String, val left: BinaryNode?, val right: BinaryNode?) : BinaryNode // Dos operandos y el símbolo de la operación ej: 2 + 3
 data class DeclarationNode(val identifier: String, val type: String): BinaryNode // Ej:  let x : number   let y: string
 data class DeclarationAssignationNode(val declaration: DeclarationNode, val assignation: BinaryNode): Assignation // Ej: let x: number = 5   let y: string = "Hello"
-data class AssignationNode(val identifier: String, val assignation: BinaryNode): Assignation // Ej: x = 2 + 3
+data class AssignationNode(val identifier: String, val assignation: BinaryNode): Assignation // Ej: x = 5       x = 2 + 3 * 4
 data class MethodNode(val identifier: String, val value: BinaryNode) : BinaryNode // Ej: print(x)
 
 
