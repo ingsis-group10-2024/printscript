5694f55d7b1c87a80b90cc6a0318dece35d90d08
RocioNavarro <56227691+RocioNavarro@users.noreply.github.com>
2024-03-30 12:50:14 -0300

    parseDeclaration tested

--- parser/src/main/kotlin/Parser.kt
+++ parser/src/main/kotlin/Parser.kt
@@ -8,48 +8,53 @@ class Parser(private val tokens: List<Token>) {
 
     private var currentTokenIndex = 0
 
-    fun generateAST(): BinaryNode? {
-        if(tokens.isEmpty()) {
-            return null
+    fun generateAST(): List<ASTNode> {
+        val nodes = mutableListOf<ASTNode>()
+        while (currentTokenIndex < tokens.size) {
+            val node = parseExpression()
+            if (node != null) {
+                nodes.add(node)
+            }
         }
-        return parseExpression()
+        return nodes
     }
 
-    private fun parseExpression(): BinaryNode? {
+    private fun parseExpression(): ASTNode? {
         return parseAddition()
     }
 
     fun parseAddition(): BinaryNode? {
         var node: BinaryNode? = parseMultiplication()
-
-        // todo voy a tener que cambiar el tokens.size porque en la lista voy a tener muchas lineas de codigo???
-        while (currentTokenIndex < tokens.size && (isCurrentToken(TokenType.PLUS) || isCurrentToken(TokenType.MINUS))) {
+        while (currentTokenIndex < tokens.size &&
+            (isCurrentToken(TokenType.PLUS) || isCurrentToken(TokenType.MINUS))) {
             val token = getTokenAndAdvance()
             val rightNode = parseMultiplication()
             node = BinaryOperationNode(token.value, node, rightNode)
         }
-
         return node
     }
 
     fun parseMultiplication(): BinaryNode? {
-        var node: BinaryNode? = parseContent()
-
+        var node: BinaryNode? = parseContent() as BinaryNode?
         while (currentTokenIndex < tokens.size &&
             (isCurrentToken(TokenType.MULTIPLY) || isCurrentToken(TokenType.DIVIDE))) {
             val token = getTokenAndAdvance()
             val rightNode = parseContent()
 
-            node = BinaryOperationNode(token.value, node, rightNode)
+            if (node != null && rightNode != null) {
+                node = BinaryOperationNode(token.value, node, rightNode as BinaryNode)
+            } else {
+                throw RuntimeException("Error parsing multiplication")
+            }
         }
-
         return node
     }
 
-    fun parseDeclaration(): BinaryNode? {
 
+
+
+    fun parseDeclaration(): DeclarationNode {
         getTokenAndAdvance()
-
         if (!isCurrentToken(TokenType.IDENTIFIER)) {
             throw RuntimeException("Expected identifier after 'let' but found: " + getCurrentToken().value)
         }
@@ -73,7 +78,7 @@ class Parser(private val tokens: List<Token>) {
         return DeclarationNode(identifier, type)
     }
 
-    private fun parseContent(): BinaryNode? {
+    private fun parseContent(): ASTNode? {
         val currentToken = getCurrentToken()
 
         return when (currentToken.type) {
@@ -96,26 +101,15 @@ class Parser(private val tokens: List<Token>) {
                 node
             }
             TokenType.LET -> {
-                // getTokenAndAdvance() por alguna razon no funciona aca
                 parseDeclaration()
             }
-            /*TokenType.STRING_LITERAL -> {
-                getTokenAndAdvance()
-                StringOperatorNode(currentToken.value)
-            }
-            TokenType.NUMERIC_LITERAL -> {
-                getTokenAndAdvance()
-                NumberOperatorNode(currentToken.value.toDouble())
-            }*/
             else -> null
         }
     }
 
-
     private fun isCurrentToken(type: TokenType): Boolean {
         return getCurrentToken().type == type
     }
-
 
     private fun getTokenAndAdvance(): Token {
         val currentToken = getCurrentToken()
@@ -126,78 +120,7 @@ class Parser(private val tokens: List<Token>) {
     private fun getCurrentToken(): Token {
         return tokens[currentTokenIndex]
     }
-
-    /*private fun insertNode(root: Node?, newNode: Node): Node {
-        if (root == null) {
-            return newNode
-        }
-
-        if (newNode < root) {
-            root.left = insertNode(root.left, newNode)
-        } else {
-            root.right = insertNode(root.right, newNode)
-        }
-
-        return root
-    }
-
-
-    *//** DECLARACION DE VARIABLES
-     *  Variables con el keyword “let”
-     *  Sin inferencia de tipos, es decir se debe aclarar el tipo de la variable (let <identifier> : <type> ; )
-     *  Ejemplo: let x: number;
-     *  Se puede declarar sólo una variable por sentencia.
-     *  Se puede declarar y asignar un valor en una misma sentencia.
-     *//*
-    private fun parseDeclaration(): DeclarationNode {
-        this.getTokenAndAdvance(TokenType.LET)
-        val declarationType = TokenType.LET
-
-        val identifierToken = this.getTokenAndAdvance(TokenType.IDENTIFIER)
-        val declarationNode = DeclarationNode(identifierToken.toString(), declarationType.toString())
-        val identifierNode = Node(identifierToken.type, headValue = identifierToken.value)
-        declarationNode.left = identifierNode
-
-        this.getTokenAndAdvance(TokenType.COLON)
-
-        val typeToken = if (getCurrentToken().type == TokenType.NUMBER_TYPE) {
-            this.getTokenAndAdvance(TokenType.NUMBER_TYPE)
-        } else {
-            this.getTokenAndAdvance(TokenType.STRING_TYPE)
-        }
-        val typeNode = Node(typeToken.type, headValue = typeToken.value)
-        declarationNode.right = typeNode
-
-        this.getTokenAndAdvance(TokenType.SEMICOLON)
-
-        return declarationNode
-    }
-
-
-    fun parsePrintlnStatement(): Node {
-        this.getTokenAndAdvance(TokenType.PRINTLN)
-        val statementType = TokenType.PRINTLN
-        val statementNode = Node(statementType, headValue = statementType.toString())
-        this.getTokenAndAdvance(TokenType.OPEN_PARENTHESIS)
-        val content = parseContent()
-        statementNode.right = content
-        this.getTokenAndAdvance(TokenType.CLOSE_PARENTHESIS)
-        this.getTokenAndAdvance(TokenType.SEMICOLON)
-
-        return statementNode
-    }
-
-    TokenType.STRING_LITERAL -> {
-                Node(currentToken.type, headValue = currentToken.value)
-            }
-            TokenType.NUMERIC_LITERAL -> {
-                Node(currentToken.type, headValue = currentToken.value)
-            }
-            TokenType.IDENTIFIER -> {
-                Node(currentToken.type, headValue = currentToken.value)
-            }
-            else -> {
-                throw RuntimeException("Token de tipo ${currentToken.type} inesperado en la línea ${currentTokenIndex}")
-            }*/
-
 }
+
+
+
--- parser/src/test/kotlin/parser/ParserTest.kt
+++ parser/src/test/kotlin/parser/ParserTest.kt
@@ -1,12 +1,10 @@
-
+import ast.ASTNode
 import ast.BinaryOperationNode
 import ast.DeclarationNode
-import ast.IdentifierOperatorNode
 import ast.NumberOperatorNode
 import common.token.Token
 import common.token.TokenType
 import org.junit.Test
-
 import parser.Parser
 import kotlin.test.assertEquals
 
@@ -15,9 +13,9 @@ class ParserTest {
     @Test
     fun testParseAddition() {
         val tokens = listOf(
-            Token(TokenType.NUMBER_TYPE, "5", 1,0),
-            Token(TokenType.PLUS, "+", 2,0),
-            Token(TokenType.NUMBER_TYPE, "3", 3,0)
+            Token(TokenType.NUMBER_TYPE, "5", 1, 0),
+            Token(TokenType.PLUS, "+", 2, 0),
+            Token(TokenType.NUMBER_TYPE, "3", 3, 0)
         )
 
         val parser = Parser(tokens)
@@ -35,9 +33,9 @@ class ParserTest {
     @Test
     fun testParseMultiplication() {
         val tokens = listOf(
-            Token(TokenType.NUMBER_TYPE, "2", 1,0),
-            Token(TokenType.MULTIPLY, "*", 2,0),
-            Token(TokenType.NUMBER_TYPE, "8", 3,0)
+            Token(TokenType.NUMBER_TYPE, "2", 1, 0),
+            Token(TokenType.MULTIPLY, "*", 2, 0),
+            Token(TokenType.NUMBER_TYPE, "8", 3, 0)
         )
 
         val parser = Parser(tokens)
@@ -55,11 +53,11 @@ class ParserTest {
     @Test
     fun testParserComplexExpression() {
         val tokens = listOf(
-            Token(TokenType.NUMBER_TYPE, "5", 1,0),
-            Token(TokenType.PLUS, "+", 2,0),
-            Token(TokenType.NUMBER_TYPE, "3", 3,0),
-            Token(TokenType.MULTIPLY, "*", 4,0),
-            Token(TokenType.NUMBER_TYPE, "2", 5,0)
+            Token(TokenType.NUMBER_TYPE, "5", 1, 0),
+            Token(TokenType.PLUS, "+", 2, 0),
+            Token(TokenType.NUMBER_TYPE, "3", 3, 0),
+            Token(TokenType.MULTIPLY, "*", 4, 0),
+            Token(TokenType.NUMBER_TYPE, "2", 5, 0)
         )
 
         val parser = Parser(tokens)
@@ -76,17 +74,17 @@ class ParserTest {
             )
         )
 
-        assertEquals(expected, result)
+        assertEquals(listOf(expected), result)
     }
 
     @Test
     fun testParseDeclaration() {
         val tokens = listOf(
-            Token(TokenType.LET, "let", 1,0),
-            Token(TokenType.IDENTIFIER, "x", 2,0),
-            Token(TokenType.COLON, ":", 3,0),
-            Token(TokenType.NUMBER_TYPE, "number", 4,0),
-            Token(TokenType.SEMICOLON, ";", 5,0)
+            Token(TokenType.LET, "let", 1, 0),
+            Token(TokenType.IDENTIFIER, "x", 2, 0),
+            Token(TokenType.COLON, ":", 3, 0),
+            Token(TokenType.NUMBER_TYPE, "number", 4, 0),
+            Token(TokenType.SEMICOLON, ";", 5, 0)
         )
 
         val parser = Parser(tokens)
@@ -96,94 +94,5 @@ class ParserTest {
 
         assertEquals(expected, result)
     }
-
-    /*@Test
-    fun testGenerateAST() {
-        val tokens = listOf(
-            Token(TokenType.LET, "let", 1),
-            Token(TokenType.IDENTIFIER, "x", 2),
-            Token(TokenType.COLON, ":", 3),
-            Token(TokenType.NUMBER_TYPE, "number", 4),
-            Token(TokenType.SEMICOLON, ";", 5)
-        )
-
-        val parser = Parser(tokens)
-        val ast = parser.generateAST()
-
-        // Verificar que el AST se haya generado correctamente
-        val expectedAST = AST(Node(TokenType.LET, null, null, "LET"))
-        val rootNode = expectedAST.getRootNode()
-        rootNode?.left = Node(TokenType.IDENTIFIER, null, null, "x")
-        rootNode?.right = Node(TokenType.NUMBER_TYPE, null, null, "number")
-
-        assertEquals(expectedAST, ast, "El AST generado no es el esperado")
-    }
-
-    @Test
-    fun testInvalidToken() {
-        val tokens = listOf(
-            Token(TokenType.LET, "let", 1),
-            Token(TokenType.IDENTIFIER, "x", 2),
-            Token(TokenType.COLON, ":", 3),
-            Token(TokenType.NUMBER_TYPE, "number", 4),
-            Token(TokenType.EQUALS, "=", 5),
-            Token(TokenType.INVALID, "$$$", 6) // Token inválido
-        )
-
-        // Verificar que el parser arroje una excepción cuando se encuentra con un token inválido
-        val parser = Parser(tokens)
-        var exceptionThrown = false
-        try {
-            parser.generateAST()
-        } catch (e: RuntimeException) {
-            exceptionThrown = true
-        }
-        assertEquals(true, exceptionThrown, "Se esperaba una excepción al encontrar un token inválido")
-    }
-
-    @Test
-    fun testParseContent() {
-        val tokens = listOf(
-            Token(TokenType.STRING_LITERAL, "\"Hello\"", 1),
-            Token(TokenType.NUMERIC_LITERAL, "42", 2),
-            Token(TokenType.IDENTIFIER, "variable", 3)
-        )
-
-        val parser = Parser(tokens)
-
-        // Verificar que el parser pueda parsear correctamente los diferentes tipos de contenido
-        val stringContent = parser.parseContent()
-        assertEquals(Node(TokenType.STRING_LITERAL, null, null, "\"Hello\""), stringContent)
-
-        val numericContent = parser.parseContent()
-        assertEquals(Node(TokenType.NUMERIC_LITERAL, null, null, "42"), numericContent)
-
-        val identifierContent = parser.parseContent()
-        assertEquals(Node(TokenType.IDENTIFIER, null, null, "variable"), identifierContent)
-    }
-
-    @Test
-    fun testParsePrintlnStatement() {
-        val tokens = listOf(
-            Token(TokenType.PRINTLN, "println", 1),
-            Token(TokenType.OPEN_PARENTHESIS, "(", 2),
-            Token(TokenType.STRING_LITERAL, "\"Hello, world!\"", 3),
-            Token(TokenType.CLOSE_PARENTHESIS, ")", 4),
-            Token(TokenType.SEMICOLON, ";", 5)
-        )
-
-        val parser = Parser(tokens)
-
-        // Verificar que el parser pueda parsear correctamente la sentencia println
-        val printlnStatement = parser.parsePrintlnStatement()
-
-        // Crear el nodo esperado para la sentencia println
-        val printlnNode = Node(TokenType.PRINTLN, null, null, "PRINTLN")
-        val stringLiteralNode = Node(TokenType.STRING_LITERAL, null, null, "\"Hello, world!\"")
-        printlnNode.right = stringLiteralNode
-
-        // Comparar el nodo obtenido con el nodo esperado
-        assertEquals(printlnNode, printlnStatement)
-    }*/
 
 }
