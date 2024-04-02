ecc2d26a42ad4ba642cd111d42df7b0072ea8f56
Apetuch <alberto.pizzi@ing.austral.edu.ar>
2024-04-02 18:51:31 -0300

    interpreter methods

--- interpreter/src/test/kotlin/InterpreterImplTest.kt
+++ interpreter/src/test/kotlin/InterpreterImplTest.kt
@@ -1,8 +1,27 @@
+import ast.*
 import org.junit.Test
 
 class InterpreterImplTest {
     @Test
     fun test001_WhenReceivingADeclarationBinaryNodeInterpreterShouldReturnItsDeclaration(){
-
+        // Arrange
+        val interpreter = InterpreterImpl()
+        val ast = DeclarationNode("x", "Int")
+        val astList = listOf(ast)
+        // Act
+        val response = interpreter.interpret(astList)
+        // Assert
+        assert(response is SuccessfulResponse)
+    }
+    @Test
+    fun test002_WhenReceivingAnAssignationBinaryNodeInterpreterShouldReturnItsAssignation(){
+        // Arrange
+        val interpreter = InterpreterImpl()
+        val ast = AssignationNode("x", NumberOperatorNode(5.0))
+        val astList = listOf(ast)
+        // Act
+        val response = interpreter.interpret(astList)
+        // Assert
+        assert(response is SuccessfulResponse)
     }
 }
