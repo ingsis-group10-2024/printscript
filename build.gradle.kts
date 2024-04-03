0c9b92671601fbaa8141a28c5f1d22bf9d5b9f0a
ccatalini <camila.catalini@ing.austral.edu.ar>
2024-04-03 14:19:56 -0300

    ojala andes sabes

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,7 @@ tasks.test {
 tasks.register<Copy>("copyPreCommitHook") {
     from(File(rootProject.rootDir, "scripts/pre-commit"))
     into(File(rootProject.rootDir, ".git/hooks"))
+    fileMode = 400
 }
 
 tasks.assemble {
