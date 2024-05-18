9a2f2720435cd474e08fa7c8e8315cd3e9f4fd9a
Apetuch <alberto.pizzi@ing.austral.edu.ar>
2024-05-18 11:48:37 -0300

    Interpreter-1.0 added to printscript-1.0

--- interpreter/src/main/kotlin/InterpreterImpl.kt
+++ interpreter/src/main/kotlin/InterpreterImpl.kt
@@ -9,19 +9,20 @@ import ast.MethodNode
 import ast.NumberOperatorNode
 import ast.StringOperatorNode
 
-class InterpreterImpl : Interpreter {
-    private val variableMap = mutableMapOf<Variable, String?>()
+class InterpreterImpl(val variableMap: VariableMap) : Interpreter {
     private val stringBuffer = StringBuffer()
 
-    override fun interpret(astList: List<ASTNode>): String? {
-        if (astList.isEmpty()) return null
+    // the Pair it returns are the variableMap and the result of the interpretation
+    override fun interpret(astList: List<ASTNode>): Pair<VariableMap, String?> {
+        if (astList.isEmpty()) return Pair(variableMap, null)
+        var varMap = variableMap
         for (ast in astList) {
             when (ast) {
                 is DeclarationNode -> {
-                    interpretDeclarationNode(ast)
+                    varMap = interpretDeclarationNode(ast)
                 }
                 is Assignation -> {
-                    interpretAssignation(ast)
+                    varMap = interpretAssignation(ast)
                 }
                 is MethodNode -> {
                     interpretMethod(ast)
@@ -38,7 +39,7 @@ class InterpreterImpl : Interpreter {
                 else -> stringBuffer.append(FailedResponse("Invalid Node Type").message)
             }
         }
-        return stringBuffer.toString()
+        return Pair(varMap, stringBuffer.toString())
     }
 
     private fun interpretMethod(ast: MethodNode) {
@@ -52,38 +53,41 @@ class InterpreterImpl : Interpreter {
         }
     }
 
-// this function will interpret the assignation node and assign the value to the variable
-    private fun interpretAssignation(ast: Assignation) {
+    // this function will interpret the assignation node and assign the value to the variable
+    private fun interpretAssignation(ast: Assignation): VariableMap {
         when (ast) {
             is DeclarationAssignationNode -> {
                 if (variableMap.containsKey(Variable(ast.declaration.identifier, ast.declaration.type))) {
                     stringBuffer.append("Variable ${ast.declaration.identifier} already declared")
-                    return
+                    return variableMap
                 }
                 val variable = Variable(ast.declaration.identifier, ast.declaration.type)
                 val value = interpretBinaryNode(ast.assignation)
-                variableMap[variable] = value
+                val newMap = variableMap.copy(variableMap = variableMap.variableMap.apply { put(variable, value) })
+                return newMap
             }
             is AssignationNode -> {
-                variableMap.keys.find { it.identifier == ast.identifier }?.let {
+                variableMap.findKey(ast.identifier)?.let {
                     val value = interpretBinaryNode(ast.assignation)
-                    variableMap[it] = value
+                    val newMap = variableMap.copy(variableMap = variableMap.variableMap.apply { put(it, value) })
                     stringBuffer.append("${it.identifier} = $value")
+                    return newMap
                 } ?: stringBuffer.append("Variable ${ast.identifier} not declared")
             }
         }
+        return variableMap
     }
 
-// this function will interpret the binary node and return the value of the node
+    // this function will interpret the binary node and return the value of the node
     private fun interpretBinaryNode(ast: ASTNode): String {
         return when (ast) {
             is NumberOperatorNode -> (ast.value).toString()
             is StringOperatorNode -> ast.value
             is IdentifierOperatorNode -> {
                 val variable =
-                    variableMap.keys.find { it.identifier == ast.identifier }
+                    variableMap.findKey(ast.identifier)
                         ?: throw Exception("Variable ${ast.identifier} not declared")
-                return variableMap[variable] ?: throw Exception("Variable ${ast.identifier} not initialized")
+                return variableMap.variableMap[variable] ?: throw Exception("Variable ${ast.identifier} not initialized")
             }
             is BinaryOperationNode -> {
                 val left = ast.left!!
@@ -101,7 +105,11 @@ class InterpreterImpl : Interpreter {
 
                             left is IdentifierOperatorNode && right is NumberOperatorNode -> {
                                 val leftValue = interpretBinaryNode(left)
-                                return leftValue + right.value
+                                return if (valueIsNumeric(leftValue)) {
+                                    (leftValue.toDouble() + right.value).toString()
+                                } else {
+                                    leftValue + right.value
+                                }
                             }
                             left is IdentifierOperatorNode && right is StringOperatorNode -> {
                                 val leftValue = interpretBinaryNode(left)
@@ -113,12 +121,27 @@ class InterpreterImpl : Interpreter {
                             }
                             left is NumberOperatorNode && right is IdentifierOperatorNode -> {
                                 val rightValue = interpretBinaryNode(right)
-                                return rightValue + left.value
+                                return if (valueIsNumeric(rightValue)) {
+                                    (left.value + rightValue.toDouble()).toString()
+                                } else {
+                                    rightValue + left.value
+                                }
                             }
                             left is IdentifierOperatorNode && right is IdentifierOperatorNode -> {
                                 val leftValue = interpretBinaryNode(left)
                                 val rightValue = interpretBinaryNode(right)
-                                return if (leftValue.toDoubleOrNull() != null && rightValue.toDoubleOrNull() != null) {
+                                return if (valueIsNumeric(leftValue) && valueIsNumeric(rightValue)) {
+                                    // Both are numbers, perform addition
+                                    (leftValue.toDouble() + rightValue.toDouble()).toString()
+                                } else {
+                                    // At least one is a string, perform concatenation
+                                    leftValue + rightValue
+                                }
+                            }
+                            left is BinaryOperationNode || right is BinaryOperationNode -> {
+                                val leftValue = interpretBinaryNode(left)
+                                val rightValue = interpretBinaryNode(right)
+                                return if (valueIsNumeric(leftValue) && valueIsNumeric(rightValue)) {
                                     // Both are numbers, perform addition
                                     (leftValue.toDouble() + rightValue.toDouble()).toString()
                                 } else {
@@ -141,6 +164,54 @@ class InterpreterImpl : Interpreter {
                                     }
                                 return result.toString()
                             }
+                            left is IdentifierOperatorNode && right is NumberOperatorNode -> {
+                                val leftValue = interpretBinaryNode(left)
+                                return if (valueIsNumeric(leftValue)) {
+                                    val result =
+                                        when (ast.symbol) {
+                                            "-" -> leftValue.toDouble() - right.value
+                                            "*" -> leftValue.toDouble() * right.value
+                                            "/" -> leftValue.toDouble() / right.value
+                                            else -> return stringBuffer.append("Invalid Operation").toString()
+                                        }
+                                    result.toString()
+                                } else {
+                                    stringBuffer.append("Invalid Operation").toString()
+                                }
+                            }
+                            left is IdentifierOperatorNode && right is IdentifierOperatorNode -> {
+                                val leftValue = interpretBinaryNode(left)
+                                val rightValue = interpretBinaryNode(right)
+                                return if (valueIsNumeric(leftValue) && valueIsNumeric(rightValue)) {
+                                    val result =
+                                        when (ast.symbol) {
+                                            "-" -> leftValue.toDouble() - rightValue.toDouble()
+                                            "*" -> leftValue.toDouble() * rightValue.toDouble()
+                                            "/" -> leftValue.toDouble() / rightValue.toDouble()
+                                            else -> return stringBuffer.append("Invalid Operation").toString()
+                                        }
+                                    result.toString()
+                                } else {
+                                    stringBuffer.append("Invalid Operation").toString()
+                                }
+                            }
+                            left is BinaryOperationNode || right is BinaryOperationNode -> {
+                                val leftValue = interpretBinaryNode(left)
+                                val rightValue = interpretBinaryNode(right)
+                                return if (valueIsNumeric(leftValue) && valueIsNumeric(rightValue)) {
+                                    val result =
+                                        when (ast.symbol) {
+                                            "-" -> leftValue.toDouble() - rightValue.toDouble()
+                                            "*" -> leftValue.toDouble() * rightValue.toDouble()
+                                            "/" -> leftValue.toDouble() / rightValue.toDouble()
+                                            else -> return stringBuffer.append("Invalid Operation").toString()
+                                        }
+                                    result.toString()
+                                } else {
+                                    stringBuffer.append("Invalid Operation").toString()
+                                }
+                            }
+
                             else -> return stringBuffer.append("Invalid Operation").toString()
                         }
                     else -> return "Invalid Operation"
@@ -153,14 +224,11 @@ class InterpreterImpl : Interpreter {
         }
     }
 
-    private fun interpretIdentifierNode(node: IdentifierOperatorNode): Any {
-        variableMap.keys.find { it.identifier == node.identifier }.let {
-            return variableMap[it]!!
-        }
-    }
+    private fun valueIsNumeric(value: String) = value.toDoubleOrNull() != null
 
-    private fun interpretDeclarationNode(ast: DeclarationNode) {
+    private fun interpretDeclarationNode(ast: DeclarationNode): VariableMap {
         // declare a variable with the given type initialized as null
-        variableMap[Variable(ast.identifier, ast.type)] = null
+        val newMap = variableMap.copy(variableMap = variableMap.variableMap.apply { put(Variable(ast.identifier, ast.type), null) })
+        return newMap
     }
 }
---
+++ interpreter/src/main/kotlin/VariableMap.kt
@@ -0,0 +1,9 @@
+data class VariableMap(val variableMap: HashMap<Variable, String?>) {
+    fun containsKey(variable: Variable): Boolean {
+        return variableMap.containsKey(variable)
+    }
+
+    fun findKey(identifier: String): Variable? {
+        return variableMap.keys.find { it.identifier == identifier }
+    }
+}
