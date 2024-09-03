43c0d56f646d7dfbf4a9ec07067d0ec83df805da
AlbertoPizzi <alberto.pizzi@ing.austral.edu.ar>
2024-09-03 01:39:41 -0300

    Fixed the const variables, now the const variables can't be modified after being made.

--- interpreter/src/main/kotlin/strategy/interpreters/AssignationInterpreterV11.kt
+++ interpreter/src/main/kotlin/strategy/interpreters/AssignationInterpreterV11.kt
@@ -6,6 +6,7 @@ import ast.AssignationNode
 import ast.DeclarationAssignationNode
 import reader.Reader
 import strategy.Interpreter
+import token.TokenType
 import variable.Variable
 import variable.VariableMap
 
@@ -20,21 +21,43 @@ class AssignationInterpreterV11(val variableMap: VariableMap, val reader: Reader
     private fun interpretAssignation(ast: Assignation): VariableMap {
         when (ast) {
             is DeclarationAssignationNode -> { // todo: checkear si es const o no.
-                if (variableMap.containsKey(Variable(ast.declaration.identifier, ast.declaration.type, true))) {
-                    return variableMap
+                if (ast.declaration.declarationType == TokenType.LET) {
+                    if (variableMap.containsKey(Variable(ast.declaration.identifier, ast.declaration.type, true))) {
+                        return variableMap
+                    }
+                    val variable = Variable(ast.declaration.identifier, ast.declaration.type, true)
+                    val value =
+                        BinaryOperationNodeInterpreterV11(
+                            variableMap,
+                            reader,
+                        ).interpret(ast.assignation)
+                    val newMap = variableMap.copy(variableMap = variableMap.variableMap.apply { put(variable, value) })
+                    return newMap
+                } else {
+                    val variable = Variable(ast.declaration.identifier, ast.declaration.type, false)
+                    val value =
+                        BinaryOperationNodeInterpreterV11(
+                            variableMap,
+                            reader,
+                        ).interpret(ast.assignation)
+                    val newMap = variableMap.copy(variableMap = variableMap.variableMap.apply { put(variable, value) })
+                    return newMap
                 }
-                val variable = Variable(ast.declaration.identifier, ast.declaration.type, true)
-                val value = BinaryOperationNodeInterpreterV11(variableMap, reader).interpret(ast.assignation)
-                val newMap = variableMap.copy(variableMap = variableMap.variableMap.apply { put(variable, value) })
-                return newMap
             }
             is AssignationNode -> {
                 variableMap.findKey(ast.identifier)?.let {
-                    val value = BinaryOperationNodeInterpreterV11(variableMap, reader).interpret(ast.assignation)
-                    val newMap = variableMap.copy(variableMap = variableMap.variableMap.apply { put(it, value) })
-                    stringBuffer.append("${it.identifier} = $value")
-
-                    return newMap
+                    if (it.isMutable) {
+                        val value =
+                            BinaryOperationNodeInterpreterV11(
+                                variableMap,
+                                reader,
+                            ).interpret(ast.assignation)
+                        val newMap = variableMap.copy(variableMap = variableMap.variableMap.apply { put(it, value) })
+                        stringBuffer.append("${it.identifier} = $value")
+                        return newMap
+                    } else {
+                        throw IllegalArgumentException("variable ${ast.identifier} is not mutable")
+                    }
                 } ?: throw IllegalArgumentException("variable ${ast.identifier} not declared")
             }
         }
