86dac741146273589e1b8a02c1f257f000b06273
ccatalini <camila.catalini@ing.austral.edu.ar>
2024-09-04 17:33:34 -0300

    build task dependencies updated

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -6,7 +6,6 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
     implementation("org.yaml:snakeyaml:2.1")
-    // testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.8.10")
     testImplementation(kotlin("test"))
 }
 
