4fd8a12e227c1889dc3744b78347d4049d6dcd99
maxim <maximiliano.maglio@ing.austral.edu.ar>
2024-09-11 14:46:15 -0300

    fix Token

--- common/src/main/kotlin/token/Token.kt
+++ common/src/main/kotlin/token/Token.kt
@@ -1,8 +1,26 @@
 package token
 
+
 data class Token(
     val type: TokenType,
     val value: String,
     val column: Int,
     val row: Int,
-)
+) {
+    // Serialización del Token a una cadena
+    override fun toString(): String {
+        return "${type.name}|$value|$column|$row"
+    }
+
+    // Deserialización del Token desde una cadena
+    companion object {
+        fun fromString(tokenString: String): Token {
+            val parts = tokenString.split("|")
+            val type = TokenType.valueOf(parts[0])
+            val value = parts[1]
+            val column = parts[2].toInt()
+            val row = parts[3].toInt()
+            return Token(type, value, column, row)
+        }
+    }
+}
